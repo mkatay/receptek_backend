ba61f0c7d6f4012f2abc8f8b7fed106edb465c8d
mkatay <kmagdi@kkando.hu>
2025-11-20 10:11:26 +0100

    cors

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const app=express()
 app.use(cors())
 app.use(express.json({limit:"5mb"}))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+
 //feltöltés végpont:
 app.post('/api/uploadImage', async (req,resp)=>{
     try {
